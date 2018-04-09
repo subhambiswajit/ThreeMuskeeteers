a9b36f5f4bc07d12842e38914ad9001139f6d259
Subham Bal <Subham_Bal@symantec.com>
2018-04-09 17:11:53 +0530

    basic layout added

--- muskeeteerClient/muskeeteerClient/urls.py
+++ muskeeteerClient/muskeeteerClient/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +9,10 @@ urlpatterns = patterns('',
     url(r'^', include('app.muskeeteer.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT, 'show_indexes': False}),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+
 )
